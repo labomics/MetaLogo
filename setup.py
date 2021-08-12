c10a5618a245f8d285d601596cf26becdc08664e
yaowenacademic <achenge07+academic@hotmail.com>
2021-08-12 14:06:12 +0800

    add long description

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ exec(open('MetaLogo/version.py').read())
 setup(name='MetaLogo',
       version=__version__,
       description='MetaLogo is a tool for making aligned sequence logos with multiple groups of sequences of different lengths or other characteristics',
+      long_description='MetaLogo is a tool for making sequence logos. Unlike other sequence logo tools, MetaLogo allows you to input sequences with different lengths or from different groups, then it can plot multiple sequence logos in one figure and align the logos to highlight the conserved patterns among different sequence groups.\
+To use MetaLogo, you could visit our public webserver http://metalogo.omicsnet.org. You could also install MetaLogo as a python package to using MetaLogo in your python scripts or in your OS terminal. If you want to provide MetaLogo to people in your local network, you could also setup a webserver by using docker.\
+Please check the tutorial for detailed usage of MetaLogo package and webserver (https://github.com/labomics/MetaLogo).',
+      long_description_content_type = 'text/plain',
       author='Yaowen Chen',
       author_email='achenge07@163.com',
       url='https://github.com/labomics/MetaLogo',
