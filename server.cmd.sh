4bbf4555b63755c5c230ee480131c45f4b6bad36
yaowenacademic <achenge07+academic@hotmail.com>
2021-11-18 17:01:14 +0800

    service status check

--- server.cmd.sh
+++ server.cmd.sh
@@ -1,3 +1,14 @@
-/etc/init.d/redis_6379 start
-supervisord -c /etc/supervisord.conf
+if [ -f "/etc/init.d/redis_6379" ]; then
+    echo "redis already running."
+else
+    /etc/init.d/redis_6379 start
+fi
+
+if pgrep -x "supervisord" > /dev/null
+then
+    echo "supervisord running"
+else
+    supervisord -c /etc/supervisord.conf
+fi
+
 GUNICORN_CMD_ARGS='--workers 10 --timeout 300 --threads 10 -b 0.0.0.0:8050 --access-logfile MetaLogo/logs/access.log --error-logfile MetaLogo/logs/err.log --capture-output --access-logformat "%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s \"%(r)s\" %(s)s %(b)s \"%(f)s\" \"%(a)s\"" ' gunicorn MetaLogo.server.index:server
